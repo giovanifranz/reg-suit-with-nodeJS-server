9016b5cb862c9239187e4b269b569d7472ece3c5
Giovani Franz <giovani.franz>
2022-04-13 13:47:17 -0300

    fix: logger

--- src/util.js
+++ src/util.js
@@ -1,7 +1,9 @@
 import pino from "pino";
+import dotenv from "dotenv";
+dotenv.config();
 
 const log = pino({
-  enabled: !process.env.LOG_DISABLED,
+  enabled: process.env.LOG_ENABLED === "true" ? true : false,
   transport: {
     target: "pino-pretty",
     options: {
